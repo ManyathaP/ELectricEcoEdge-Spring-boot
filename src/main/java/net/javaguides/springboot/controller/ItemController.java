20553a949b7dddb6665decb9f8f979200d6073f7
Punu N Gowda <115967261+ManyathaP@users.noreply.github.com>
2024-07-16 10:08:10 +0530

    e

--- src/main/java/net/javaguides/springboot/controller/ItemController.java
+++ src/main/java/net/javaguides/springboot/controller/ItemController.java
@@ -27,7 +27,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/items")
 public class ItemController {
