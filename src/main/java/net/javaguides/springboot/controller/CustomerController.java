abf4fed3ca34aad1f452b593f3edd39102c7a017
Punu N Gowda <115967261+ManyathaP@users.noreply.github.com>
2024-07-16 10:05:56 +0530

    d

--- src/main/java/net/javaguides/springboot/controller/CustomerController.java
+++ src/main/java/net/javaguides/springboot/controller/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springboot.model.Signup;
 import net.javaguides.springboot.repository.CustomerRepository;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
