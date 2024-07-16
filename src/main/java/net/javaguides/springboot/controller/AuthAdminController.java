7ea4b0d71d870be031d26b08241798dd94dd5329
Punu N Gowda <115967261+ManyathaP@users.noreply.github.com>
2024-07-16 10:03:19 +0530

    b

--- src/main/java/net/javaguides/springboot/controller/AuthAdminController.java
+++ src/main/java/net/javaguides/springboot/controller/AuthAdminController.java
@@ -12,7 +12,7 @@ import net.javaguides.springboot.model.Admin;
 import net.javaguides.springboot.repository.AdminRepository;
 import net.javaguides.springboot.service.EmailService;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/authadmin")
 public class AuthAdminController{
