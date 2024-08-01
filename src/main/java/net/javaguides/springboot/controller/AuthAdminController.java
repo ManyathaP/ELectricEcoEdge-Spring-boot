75c7778796f590c01bed8c03d7c3b0a3fa589755
ManyathaP <115967261+ManyathaP@users.noreply.github.com>
2024-08-01 19:27:38 +0530

    Update AuthAdminController.java

--- src/main/java/net/javaguides/springboot/controller/AuthAdminController.java
+++ src/main/java/net/javaguides/springboot/controller/AuthAdminController.java
@@ -12,7 +12,7 @@ import net.javaguides.springboot.model.Admin;
 import net.javaguides.springboot.repository.AdminRepository;
 import net.javaguides.springboot.service.EmailService;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "https://https://electric-eco-edge-app.vercel.app")
 @RestController
 @RequestMapping("/api/v1/authadmin")
 public class AuthAdminController{
