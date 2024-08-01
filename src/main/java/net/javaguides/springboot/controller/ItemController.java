656bb34d40d1ce9421b1f2c243aa5ec6c7381921
ManyathaP <115967261+ManyathaP@users.noreply.github.com>
2024-08-01 19:29:30 +0530

    Update ItemController.java

--- src/main/java/net/javaguides/springboot/controller/ItemController.java
+++ src/main/java/net/javaguides/springboot/controller/ItemController.java
@@ -27,7 +27,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "https://https://electric-eco-edge-app.vercel.app")
 @RestController
 @RequestMapping("/api/v1/items")
 public class ItemController {
